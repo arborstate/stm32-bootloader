a6bde5199d3ef8a5dd6035afa6b6fedaefcb63d9
Alexander Guy <alexander@andern.org>
2019-04-17 15:03:44 -0700

    Clock games.  Play around with the UART.

--- main.c
+++ main.c
@@ -7,9 +7,13 @@
 bxcan_state_t caniface;
 
 #define _FCK 8000000U
+
+#define _SET_REG(reg, field, val) reg = (reg & ~(field ## _Msk)) | (val << (field ## _Pos))
+
 void
 main(void)
 {
+
 	{
 		// Turn on some important hardware.
 		RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOFEN | RCC_AHBENR_CRCEN;
@@ -28,20 +32,21 @@ main(void)
 		RCC->CR &= ~RCC_CR_PLLON;
 
 		// Attach PLL to HSE/2 * 9;
-		RCC->CFGR &= ~RCC_CFGR_PLLXTPRE_Msk;
-		RCC->CFGR |= RCC_CFGR_PLLSRC_HSE_PREDIV;
-		RCC->CFGR |= RCC_CFGR_PLLMUL9;
+		_SET_REG(RCC->CFGR2, RCC_CFGR2_PREDIV, 1);
+		_SET_REG(RCC->CFGR, RCC_CFGR_PLLMUL, 7);
+		_SET_REG(RCC->CFGR, RCC_CFGR_PLLSRC, 1);
 
 		RCC->CR |= RCC_CR_PLLON;
 		do {} while(!(RCC->CR & RCC_CR_PLLRDY));
+
+		_SET_REG(RCC->CFGR, RCC_CFGR_SW, 1);
+
 	}
 
 	// Setup USART3
 	{
-		RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
 		RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
-
-		// TX on PC10
+                // TX on PC10
 		_gpio_afr(GPIOC, 10, 7);
 		_gpio_moder(GPIOC, 10, 0x2);
 
@@ -49,18 +54,24 @@ main(void)
 		_gpio_afr(GPIOC, 11, 7);
 		_gpio_moder(GPIOC, 11, 0x2);
 
+		// Enable the USART3 clock.
+		RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
 		// Set the outgoing bit rate.
 		USART3->BRR = _FCK / 38400;
 
-		// Turn on the transmitter and USART.
-		USART3->CR1 = USART_CR1_TE | USART_CR1_UE;
+		// Turn on the transmitter.
+		// Turn on the USART.
+		USART3->CR1 |= USART_CR1_UE | USART_CR1_TE;
 
-		for (int i = 0; i < 16; i++) {
-			do {} while (!(USART3->ISR & USART_ISR_TXE));
-			USART3->TDR = 'A';
+		const char *_msg = "hey you guys! ";
+
+		while (1) {
+			for (int i = 0; i < 14; i++) {
+				do {} while (!(USART3->ISR & USART_ISR_TXE));
+				USART3->TDR = _msg[i];
+			}
 		}
 	}
-
 
 	// Setup the CAN interface.
 	{
